6b39a578d6e00249cf2327b3bf7ef9a94d54f9e8
paulotassi <133156624+paulotassi@users.noreply.github.com>
2024-10-08 00:28:14 -0400

    Adjust Canvas and Created the game foundation with character mover

--- Assets/Scripts/Items/collectableObject.cs
+++ Assets/Scripts/Items/collectableObject.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class collectableObject : MonoBehaviour
 {
+    [SerializeField] public int damageValue;
     public virtual void Player1CollectItem()
     {
 
@@ -18,10 +19,13 @@ public class collectableObject : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             Player1CollectItem();
+            collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(damageValue);
         }
         else if (collision.gameObject.tag == "Player2")
         {
             Player2CollectItem();
+            collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(damageValue);
         }
+        Destroy(gameObject);
     }
 }
