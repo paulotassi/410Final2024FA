644c77f03cfb382210b89c33261b3076fd94ec23
paulotassi <133156624+paulotassi@users.noreply.github.com>
2024-10-07 23:26:28 -0400

    MadeCollectableIngredient

---
+++ Assets/Scripts/Items/collectableObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectableObject : MonoBehaviour
+{
+    public virtual void Player1CollectItem()
+    {
+
+    }
+    public virtual void Player2CollectItem() 
+    { 
+    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Player1CollectItem();
+        }
+        else if (collision.gameObject.tag == "Player2")
+        {
+            Player2CollectItem();
+        }
+    }
+}
--- Assets/Scripts/ManagerScripts/GameManager.cs
+++ Assets/Scripts/ManagerScripts/GameManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TMP_Text Player1ScoreText;
+    [SerializeField] private TMP_Text Player2ScoreText;
+    [SerializeField] public int player1IngredientCount = 0;
+    [SerializeField] public int player2IngredientCount = 0;
 
-    // Update is called once per frame
+// Start is called before the first frame update
+
     void Update()
     {
-        
+        Player1ScoreText.text = "Ingredients Collected: " + player1IngredientCount;
+        Player2ScoreText.text = "Ingredients Collected: " + player2IngredientCount;
+    }
+
+    public void player1IncreaseIngredient()
+    {
+        player2IngredientCount++;
+    }
+    public void player2IncreaseIngredient()
+    {
+        player2IngredientCount++;
     }
 }
