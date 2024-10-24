e2d9260eb62753bbaec32d895b7a711b879a182a
paulotassi <133156624+paulotassi@users.noreply.github.com>
2024-10-24 00:40:46 -0400

    Screenshake and Dash fix

--- Assets/Scripts/Player/Projectile/projectile.cs
+++ Assets/Scripts/Player/Projectile/projectile.cs
@@ -6,19 +6,27 @@ public class projectile : MonoBehaviour
 {
 
     public float projectileSpeed;
-    public float projectileDamage;
+    public int projectileDamage = 10;
     public Rigidbody2D rb;
+    [SerializeField] GameManager gameManager;
 
     private void Start()
     {
        rb = this.gameObject.GetComponent<Rigidbody2D>();
        rb.velocity = transform.right * projectileSpeed;
+        gameManager = FindFirstObjectByType<GameManager>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.gameObject.GetComponent<PlayerController>())
         {
-            Destroy(gameObject);
+            Destroy(this.gameObject);
+        }
+        else if (collision.gameObject.GetComponent<PlayerController>() != null && gameManager.competetiveMode)
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(projectileDamage);
+            //Debug.Log("Hit a Player");
+            Destroy(this.gameObject);
         }
     }
 
