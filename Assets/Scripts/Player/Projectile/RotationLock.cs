e3136dc9c8c8e8b4a97ee3752a551fee69de6742
paulotassi <133156624+paulotassi@users.noreply.github.com>
2025-02-25 22:04:52 -0500

    Progress commit. Going to Switch and work on MVP

--- Assets/Scripts/Player/Projectile/RotationLock.cs
+++ Assets/Scripts/Player/Projectile/RotationLock.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class RotationLock : MonoBehaviour
+public class RotationLock : NetworkBehaviour
 {
 
     void Update()
