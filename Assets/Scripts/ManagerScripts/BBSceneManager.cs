ca38219e93af817bc75e28df21cb7294d51fd9ac
Taylor Joslin <143643796+TaylorJoslin@users.noreply.github.com>
2025-04-03 10:48:38 -0400

    Fixed button highlight issue

--- Assets/Scripts/ManagerScripts/BBSceneManager.cs
+++ Assets/Scripts/ManagerScripts/BBSceneManager.cs
@@ -7,6 +7,11 @@ public class BBSceneManager : MonoBehaviour
 {
     //Eventual intent is to make the play to go as follows. Player hits initial button choice bringing you to either coop gamestate. when players finish a round they returnt to titlescreen and the button changes from coopmode to continue coop
     //A state will exist that will continue guiding players through their playthrough. an additional button will appear that will say to reset coop run or reset comp run to bring player game states back to first level.
+    public void LoadPlaythrough1()
+    {
+        SceneManager.LoadScene("CompetetiveSceneRound1");
+    }
+
     public void LoadCompetetivePlaythrough()
     {
         SceneManager.LoadScene("CompetitiveLevel2");
