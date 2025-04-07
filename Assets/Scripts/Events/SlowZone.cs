f6464d11ee80fee8d03e01cb7878849d7050dd49
paulotassi <133156624+paulotassi@users.noreply.github.com>
2025-04-06 23:06:03 -0400

    fixed doubleslow effects

--- Assets/Scripts/Events/SlowZone.cs
+++ Assets/Scripts/Events/SlowZone.cs
@@ -4,33 +4,28 @@ public class SlowZone : MonoBehaviour
 {
     public float slowAmount = 4;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         var playV = collision.GetComponent<PlayerController>();
         
         if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Player2")
         {   
-            float limitedSlow = playV.topSpeed/slowAmount;
-            if (playV.topSpeed > limitedSlow)
-            {
-                Debug.Log(collision.name + " Slowed");
-                playV.topSpeed /= slowAmount;
-                playV.flightSpeed /= slowAmount;
-                playV.moveHorizontalFlightSpeed /= slowAmount;
-            }
+
+                playV.playerSlowed(slowAmount);
+
 
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        var playV = collision.GetComponent<PlayerController>();
         if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Player2")
         {
-            var playV = collision.GetComponent<PlayerController>();
-            playV.topSpeed *= slowAmount;
-            playV.flightSpeed *= slowAmount;
-            playV.moveHorizontalFlightSpeed *= slowAmount;
+            playV.playerUnslowed(slowAmount);
 
         }
     }
+
+    
 }
