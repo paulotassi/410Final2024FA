955f7452cb82d0f36bf4f00db42efa2324653f9d
Taylor Joslin <143643796+TaylorJoslin@users.noreply.github.com>
2025-04-10 01:07:52 -0400

    TitleScreenEdits
    -Created a Game Settings static script to handle  game modes
    -Updated the game manager with new Game Settings script
    -Created SinglePlayer and TwoPlayer buttons
    -Rearranged title screen buttons and added horizontal layouts

--- Assets/Scripts/ManagerScripts/BBSceneManager.cs
+++ Assets/Scripts/ManagerScripts/BBSceneManager.cs
@@ -5,8 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class BBSceneManager : MonoBehaviour
 {
+    [SerializeField] public GameObject SinglePlayer;
+    [SerializeField] public GameObject TwoPlayer;
+
+    [SerializeField] public GameObject LevelSelect;
+    [SerializeField] public GameObject Versus;
+    [SerializeField] public GameObject Credits;
+    [SerializeField] public GameObject Exit;
+    [SerializeField] public GameObject BackButton;
+
+
     //Eventual intent is to make the play to go as follows. Player hits initial button choice bringing you to either coop gamestate. when players finish a round they returnt to titlescreen and the button changes from coopmode to continue coop
     //A state will exist that will continue guiding players through their playthrough. an additional button will appear that will say to reset coop run or reset comp run to bring player game states back to first level.
+    private void Start()
+    {
+        LevelSelect.SetActive(false);
+        Versus.SetActive(false);
+        Credits.SetActive(false);
+        Exit.SetActive(false);
+        BackButton.SetActive(false);
+    }
     public void LoadCoopPlaythrough1()
     {
         SceneManager.LoadScene("CompetetiveLevel1");
@@ -31,4 +49,50 @@ public class BBSceneManager : MonoBehaviour
     {
         Application.Quit();
     }
+    public void StartSinglePlayerMode()
+    {
+        GameSettings.singlePlayerMode = true;
+        GameSettings.competetiveMode = false;
+        GameSettings.arcadeMode = false;
+
+        LevelSelect.SetActive(true);
+        Versus.SetActive(false);
+        Credits.SetActive(true);
+        Exit.SetActive(true);
+        BackButton.SetActive(true);
+        SinglePlayer.SetActive(false);
+        TwoPlayer.SetActive(false);
+
+    }
+
+    public void StartTwoPlayerMode()
+    {
+        GameSettings.singlePlayerMode = false;
+        GameSettings.competetiveMode = true;
+        GameSettings.arcadeMode = false;
+
+        LevelSelect.SetActive(true);
+        Versus.SetActive(true);
+        Credits.SetActive(true);
+        Exit.SetActive(true);
+        BackButton.SetActive(true);
+        SinglePlayer.SetActive(false);
+        TwoPlayer.SetActive(false);
+    }
+
+    public void Back()
+    {
+        GameSettings.singlePlayerMode = false;
+        GameSettings.competetiveMode = false;
+        GameSettings.arcadeMode = false;
+
+        LevelSelect.SetActive(false);
+        Versus.SetActive(false);
+        Credits.SetActive(false);
+        Exit.SetActive(false);
+        BackButton.SetActive(false);
+        SinglePlayer.SetActive(true);
+        TwoPlayer.SetActive(true);
+    }
+
 }
---
+++ Assets/Scripts/ManagerScripts/GameSettings.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public static class GameSettings 
+{
+    public static bool singlePlayerMode = false;
+    public static bool competetiveMode = false;
+    public static bool arcadeMode = false;
+}
