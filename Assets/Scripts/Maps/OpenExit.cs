4467c449d83d10a9be76e86ba611fda9ef43d9fb
paulotassi <133156624+paulotassi@users.noreply.github.com>
2026-05-18 11:50:05 -0400

    Working Early PCG

--- Assets/Scripts/Maps/OpenExit.cs
+++ Assets/Scripts/Maps/OpenExit.cs
@@ -12,6 +12,7 @@ public class OpenExit
     }
 }
 
+
 public enum Direction
 {
     North,
--- Assets/Scripts/Maps/TileData.cs
+++ Assets/Scripts/Maps/TileData.cs
@@ -7,5 +7,6 @@ public class TileData : ScriptableObject
     public string tileName;
     public GameObject tilePrefab;
     public Vector2Int size = Vector2Int.one;
+    public Vector2[] exitVectors = new Vector2[4];
     public List<Direction> exits;  // The directions this tile connects to (e.g., North and East)
 }
